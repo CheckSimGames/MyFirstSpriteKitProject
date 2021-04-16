16010c026f2751e22b9a43a848a09ce32d940f4e
Mark Szymczyk <mark@meandmark.com>
2021-04-16 16:15:57 -0400

    Initial Commit

---
+++ MyFirstSpriteKitProject macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MyFirstSpriteKitProject macOS
+//
+//  Created by mark on 4/16/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
